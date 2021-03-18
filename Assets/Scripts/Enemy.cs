8bd1f107676be75a34ce050ffdc82727b9a7ca36
unknown <aliemreonur@gmail.com>
2021-03-19 00:21:52 +0300

    Enemy destroyed with bullet or player

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float _enemySpeed = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _enemySpeed * Time.deltaTime);
+        
+        if(transform.position.y < -5.5f)
+        {
+            float randomX = Random.Range(-9, 9);
+            transform.position = new Vector3(randomX, 5.5f, 0);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+
+        }
+
+        else if(other.gameObject.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
